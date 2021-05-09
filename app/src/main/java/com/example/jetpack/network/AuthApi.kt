4c8cf83021055372f986963d41b0f7b0ae3546ef
Masum Modi <masummodi1994@gmail.com>
2021-05-08 21:49:53 -0400

    feat: auth design and retrofit setup

---
+++ app/src/main/java/com/example/jetpack/network/AuthApi.kt
@@ -0,0 +1,17 @@
+package com.example.jetpack.network
+
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface AuthApi {
+
+    @FormUrlEncoded
+    @POST("auth/login")
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Any
+
+
+}
